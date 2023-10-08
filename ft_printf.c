35140aa146d06923b4ea82cc9dc078c50835f920
Franco Stefano Curulla Galan <fracurul@c3r3s4.42malaga.com>
2023-10-08 19:15:15 +0200

    prinft

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.c                                           :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/05 19:58:26 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/05 19:58:29 by fracurul         ###   ########.fr       */
+/*   Created: 2023/10/08 19:13:44 by fracurul          #+#    #+#             */
+/*   Updated: 2023/10/08 19:13:47 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+int	ft_printf(char *s, ...)
+{
+	size_t	i;
+	va_list	pro;
+
+	va_start(pro, s);
+	i = va_arg(pro, &s[i]);
+	/*i =  -1;
+	while (s[++i])
+		write(1, &s[i], 1);
+	*/
+	va_end(pro);
+	return (i);
+}
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i", ft_printf("", 200));
+}
