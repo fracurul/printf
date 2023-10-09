3d658ba1662e4938833c51a5b44bad225f6ed657
fracurul <145069899+fracurul@users.noreply.github.com>
2023-10-09 20:01:25 +0200

    utils

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -5,8 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/08 18:28:29 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/08 18:51:39 by fracurul         ###   ########.fr       */
+/*   Created: 2023/10/05 20:34:07 by fracurul          #+#    #+#             */
+/*   Updated: 2023/10/09 19:45:34 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+
+void    ft_putchar(char c, size_t *counter)
+{
+    write(1, &c, 1);
+    counter++;
+}
+
+
+void    ft_putstr(char *str, size_t counter)
+{
+    size_t  i;
+    
+    i = -1;
+    while(str[++i])
+    {
+        write(1, &str[i], 1);
+        counter++;
+    }
+}
+
+size_t  ft_percent(void)
+{
+    write(1, "%", 1);
+    return(1);
+}
