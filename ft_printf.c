874aa3aaa2b0c7a845a34193b56ad978ebc421f0
fracurul <fracurul@42Ubuntu.myguest.virtualbox.org>
2023-10-11 21:02:12 +0200

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,23 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 19:13:44 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/09 21:26:12 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/10/11 17:19:41 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void   ft_format(va_list va, char *s, size_t *counter)
+void   ft_format(va_list va, char *s, size_t *bytecnt)
 {
     if(s == 'c')
-        ft_putchar(va_arg(va, int), counter);
+        ft_putchar(va_arg(va, int), bytecnt);
     else if (s == 's')
-        ft_putstr(va_arg(va, char *), counter);
+        ft_putstr(va_arg(va, char *), bytecnt);
     else if(s == 'p')
+    else if((s == 'd')
+    else if(s == 'i')
+        ft_printnbr(va_arg(va, int), bytecnt);
+    
         
         
 }
@@ -26,18 +30,18 @@ void   ft_format(va_list va, char *s, size_t *counter)
 int	ft_printf(char *s, ...)
 {
 	size_t	i;
-    size_t  counter;
+    size_t  bytecnt;
 	va_list	formats;
 
 	va_start(formats, s);
 	i =  -1;
-    counter = 0;
+    bytecnt = 0;
 	while (s[++i])
     {
 		if (s[i] == '%')
         {
             i++;
-            ft_format(formats, s[i], counter);
+            ft_format(formats, s[i], bytecnt);
         }
 
     }    
