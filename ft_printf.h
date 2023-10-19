1fd4918ba178a5bdb0fe05c61bc88e559ac51b41
root <root@42Ubuntu.myguest.virtualbox.org>
2023-10-19 17:39:59 +0200

    library

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 20:25:51 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/16 17:48:58 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/10/19 17:37:43 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,10 @@
 int     ft_printf(char *s, ...);
 void    ft_printchar(char c, size_t *bytecnt);
 void    ft_printstr(char *str, size_t *bytecnt);
-void ft_printnb(int long nb, size_t *bytecnt);
+void    ft_printnb(int long nb, size_t *bytecnt);
 void    ft_printbase(unsigned long long nb, char *base, size_t *bytecnt);
 void    ft_printpnt(unsigned long long pnt, size_t *bytecnt);
 size_t  ft_percent(void);
 size_t  ft_strlen(char *s);
-
-
 
 #endif
