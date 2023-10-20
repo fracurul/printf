d4fa151a436b9f157536d2d97dd66e54b1dd34f8
Franco Stefano Curulla Galan <fracurul@c3r6s3.42malaga.com>
2023-10-20 19:41:34 +0200

    utils

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,49 +6,50 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 20:34:07 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/19 17:38:05 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:37:09 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void    ft_printchar(char c, size_t *bytecnt)
+void	ft_printchar(char c, int *bytecnt)
 {
-    write(1, &c, 1);
-    bytecnt++;
+	int	protect;
+
+	protect = write(1, &c, 1);
+	*bytecnt = *bytecnt + 1;
+	if (protect == -1)
+		*bytecnt = -1;
 }
 
-void    ft_printstr(char *str, size_t *bytecnt)
+void	ft_printstr(char *str, int *bytecnt)
 {
-    size_t  i;
-    
-    i = -1;
-    while(str[++i])
-    {
-        write(1, &str[i], 1);
-        bytecnt++;
-    }
+	size_t	i;
+
+	i = -1;
+	if (!str)
+		ft_printstr("(null)", bytecnt);
+	while (str && str[++i])
+		ft_printchar(str[i], bytecnt);
 }
 
-void ft_printnb(int long nb, size_t *bytecnt)
+void	ft_printnb(int long nb, int *bytecnt)
 {
-    if(nb < 0)
-    {
-        ft_printchar('-', bytecnt);
-        nb *= -1;
-        
-    }
-    if(nb > 9)
-    {
-        ft_printnb(nb / 10, bytecnt);
-        ft_printnb(nb % 10, bytecnt);
-    }
-    else
-        ft_printchar(nb + '0', bytecnt);
+	if (nb < 0)
+	{
+		ft_printchar('-', bytecnt);
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_printnb(nb / 10, bytecnt);
+		ft_printnb(nb % 10, bytecnt);
+	}
+	else
+		ft_printchar(nb + '0', bytecnt);
 }
 
-size_t  ft_percent(void)
+void	ft_percent(int *bytecnt)
 {
-    write(1, "%", 1);
-    return(1);
+	ft_printchar('%', bytecnt);
 }
--- ft_printf_utils2.c
+++ ft_printf_utils2.c
@@ -6,38 +6,38 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 17:09:32 by fracurul          #+#    #+#             */
-/*   Updated: 2023/10/19 17:39:02 by fracurul         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:35:23 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-size_t  ft_strlen(char *s)
+static size_t	ft_strlen(char *s)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while(s[i])
-        i++;
-    return(i);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-void    ft_printbase(unsigned long long nb, char *base, size_t *bytecnt)
+void	ft_printbase(unsigned long long nb, char *base, int *bytecnt)
 {
-    if (nb >= ft_strlen(base))
-    {
-        ft_printbase(nb / ft_strlen(base), base, bytecnt);
-        ft_printbase(nb % ft_strlen(base), base, bytecnt);
-    }
-    else
-        ft_printchar(nb +'0', bytecnt);
+	if (nb >= ft_strlen(base))
+	{
+		ft_printbase(nb / ft_strlen(base), base, bytecnt);
+		ft_printbase(nb % ft_strlen(base), base, bytecnt);
+	}
+	else
+		ft_printchar(base[nb], bytecnt);
 }
 
-void    ft_printpnt(unsigned long long pnt, size_t *bytecnt)
+void	ft_printpnt(unsigned long long pnt, int *bytecnt)
 {
-    ft_printstr("0x", *bytecnt);
-    if (pnt >= 9223372036854775807)
-        ft_printbase(pnt, "0123456789abcdef", bytecnt);
-    else
-        ft_printbase(pnt, "0123456789abcdef", bytecnt);
+	ft_printstr("0x", bytecnt);
+	if (pnt >= 9223372036854775807)
+		ft_printbase(pnt, "0123456789abcdef", bytecnt);
+	else
+		ft_printbase(pnt, "0123456789abcdef", bytecnt);
 }
